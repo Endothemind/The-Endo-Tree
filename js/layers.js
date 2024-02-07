99258f10a341f81942d8a13b584fa46cc714d0e9
Endo <149655841+Endothemind@users.noreply.github.com>
2024-02-06 19:04:19 -0600

    ahhh

--- js/layers.js
+++ js/layers.js
@@ -7,6 +7,7 @@ addLayer("e", {
 		points: new Decimal(0),
     }},
     color: "#4BDC13",
+    branches: "S",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "Exponents", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
@@ -67,5 +68,38 @@ addLayer("e", {
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }
         },
+    },
+})
+
+
+addLayer("s", {
+    name: "Square", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "S", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    color: "#4248f5",
+    requires: new Decimal(2000), // Can be a function that takes requirement increases into account
+    resource: "Square", // Name of prestige currency
+    baseResource: "Exponents", // Name of resource prestige is based on
+    baseAmount() {return player.square}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "e", description: "e: Reset for Exponents", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true},
+    upgrades: {
+      
     },
 })
