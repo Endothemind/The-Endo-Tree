0cd235f9c313ccf70fb5dd9d72eea6758aa40279
Endo <149655841+Endothemind@users.noreply.github.com>
2024-02-06 18:52:12 -0600

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -63,7 +63,7 @@ addLayer("e", {
             description: "Exponents and numbers are too confusing!",
             cost: new Decimal(250),
             effect() {
-                return player[this.layer].points.add(1).pow(0.25)
+                return player[this.layer].points.add(1).pow(0.075)
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }
         },
